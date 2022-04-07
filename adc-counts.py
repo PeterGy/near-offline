63ce85710cb2034c295ebbd8f6d63799cf28d37e
PeterGy <petigyorgy@gmail.com>
2022-04-07 11:58:06 +0100

    Implemented pdf plottting

--- adc-counts.py
+++ adc-counts.py
@@ -2,50 +2,44 @@
 #Decode a x.raw files using decode.py
 #Use the following to get a plot of ADCS, with no pedestals subtracted:
 # ldmx python3 this_file.py interesting_run.root
-#Use the following to get a plot of ADCS, with the pedestal subtracted:
-# ldmx python3 this_file.py interesting_run.root an_empty_run.root
 
 #A program that plots the adc counts for each channel
-#So far untested. Expected to work when all HGCROCs are connected
 
 from mapping import *
 #keeps only events we are interested in
 eventsOfInterest = range(4,5)
-channelsOfInterest = range(1,40)
+channelsOfInterest = range(0,40)
 
 #prepares the plots
 inputFile=r.TFile(sys.argv[1], "read")
 NumberOfChannels=384
 r.gStyle.SetOptStat("ne")
 hists =   [r.TH1F(str(channel), "Pulse shape for SiPM",256, 0, 256) for channel in range(0,NumberOfChannels)]
-# hists =   [r.TH1F(str(channel), "Pulse shape for SiPM",512, -256, 256) for channel in range(0,NumberOfChannels)]
-pedestals =   [0 for channel in range(0,NumberOfChannels)]
 allData=inputFile.Get('ntuplizehgcroc').Get("hgcroc") #
 
-#Gets data from interesting sample, already subtracting pedestals
+#Gets data from interesting events
 for t in allData : #for timestamp in allData
     if t.event in eventsOfInterest:
-        # if t.i_sample==1: #we define event ADCs as the second time sample for now
-            realChannel = FpgaLinkChannel_to_realChannel([t.fpga,t.link,t.channel])
-            #fpga -2 because of a board quirk I am told not to worry about for this specific sample
-            if realChannel != None: hists[realChannel].Fill(t.adc-pedestals[realChannel])
+        realChannel = FpgaLinkChannel_to_realChannel([t.fpga,t.link,t.channel])
+        if realChannel != None: hists[realChannel].Fill(t.adc)
 
 #makes the histograms
+canvases=[r.TCanvas(str(i+1),'The canvas of anything', 250, 1200) for i in range(0,19)]
+for c in canvases: c.Divide(2,12)
+
+r.gStyle.SetLineScalePS(0.3)
 for hist in hists: 
     if int(hist.GetName()) in channelsOfInterest:
-        c =r.TCanvas('c','The canvas of anything', 1100, 900)
-        hist.SetYTitle('event counts')
+        l= realChannel_to_SipM(int(hist.GetName()))
+        c =canvases[l[0]-1]
+        c.cd(l[1]*2+l[2]+1)
+        hist.SetYTitle('timestamp counts')
         hist.SetXTitle('ADC')
         hist.SetMinimum(0)
         hist.SetLineColor(4)
+        hist.SetLineWidth(1)
         hist.Draw('same')
         label = r.TLatex()
         label.SetNDC()  
-        label.DrawLatex(0,  0.92, "This is SiPM "+str(realChannel_to_SipM(int(hist.GetName()))))
-        if (len(eventsOfInterest)==1): 
-            label.DrawLatex(0,  0.85, "This is event "+str(eventsOfInterest[0]))                
-            c.SaveAs("plots/adc-counts_events-"+str(eventsOfInterest[0])+"_channel-"+hist.GetName()+".png" ) 
-        else:
-            c.SaveAs("plots/adc-counts_channel-"+hist.GetName()+".png")  
-        c.Close()
-
+        label.DrawLatex(0,  0.91, "SiPM of layer "+str(l[0])+", bar "+str(l[1])+", side "+str(l[2])  )
+for c in canvases: c.SaveAs("plots/adc-counts_layer-"+c.GetName()+".pdf") 
